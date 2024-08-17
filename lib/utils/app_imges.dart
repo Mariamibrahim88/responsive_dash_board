224f80e4206f51682eccc16bbc00b36df0e1819b
maria <mariamibrahimmi4142726@gmail.com>
2024-08-17 23:53:08 +0300

    add fonts and images

---
+++ lib/utils/app_imges.dart
@@ -0,0 +1,64 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar2.svg
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCard
+  /// assets/images/card.svg
+  static const String imagesCard = "assets/images/card.svg";
+
+  /// Assets for imagesCategry
+  /// assets/images/categry.svg
+  static const String imagesCategry = "assets/images/categry.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesInvestment
+  /// assets/images/investment.svg
+  static const String imagesInvestment = "assets/images/investment.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMytransaction
+  /// assets/images/mytransaction.svg
+  static const String imagesMytransaction = "assets/images/mytransaction.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatics
+  /// assets/images/statics.svg
+  static const String imagesStatics = "assets/images/statics.svg";
+
+  /// Assets for imagesTransaction
+  /// assets/images/transaction.zip
+  static const String imagesTransaction = "assets/images/transaction.zip";
+
+  /// Assets for imagesVuesaxTwotoneConvertCard
+  /// assets/images/vuesax/twotone/convert-card.svg
+  static const String imagesVuesaxTwotoneConvertCard = "assets/images/vuesax/twotone/convert-card.svg";
+
+  /// Assets for imagesWallet
+  /// assets/images/wallet.svg
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
